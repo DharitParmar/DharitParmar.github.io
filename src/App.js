aefe69ae6da002aa5b91289e93f3a41ea40a34a9
Parmar, Dharit <dharit.parmar@one.verizon.com>
2020-12-26 16:40:13 -0500

    correction for under const. msg

--- src/App.js
+++ src/App.js
@@ -5,9 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h2 className="App-header-title">
+          <b>DNotes |</b>
+          <u>Dharit parmar's personal blog</u>
+        </h2>
         <a>
-          Bolg is under construction, till then use my connection handles below. 
+          Under construction, till then connect to Dharit through his linkedin handle below. 
         </a>
         <a className="App-link"
           href="https://www.linkedin.com/in/dharit-parmar/">Connect over linked-in
